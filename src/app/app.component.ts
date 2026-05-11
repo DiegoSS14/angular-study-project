9803f080c90190dacfd8071f9ed0d7b96e558dac
unknown <sousadiego533@gmail.com>
2026-05-10 22:56:35 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CardPurpleComponent } from './card-purple/card.component';
+import { CardModule } from './card/card.module';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CardModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ src/app/card-purple/card.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { ButtonPurpleComponent } from '../button-purple/button.component';
+
+@Component({
+  selector: 'app-card-purple',
+  imports: [ButtonPurpleComponent],
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.scss'
+})
+export class CardPurpleComponent {
+  
+}

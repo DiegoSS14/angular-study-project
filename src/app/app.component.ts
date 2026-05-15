dc54d2fa6417c3fd2a0ab894803119ce6cf054dc
unknown <sousadiego533@gmail.com>
2026-05-15 02:02:34 -0300

    Adiciona componente input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CardModule } from './card/card.module';
 
@@ -10,4 +10,11 @@ import { CardModule } from './card/card.module';
 })
 export class AppComponent {
   title = 'componentProject';
+  input = ''
+
+  onInput(event: Event) {
+    let text = (event.target as HTMLInputElement).value
+    // this.input = text
+    console.log(text)
+  }
 }
